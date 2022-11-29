c532a8d01433e7e4f8e31129a56591b5bd9ad35c
BrandenWilliams <BrandenWilliams07@gmail.com>
2022-11-28 18:50:48 -0800

    remove title from entry

--- plugins/tasklists/taskEntry.go
+++ plugins/tasklists/taskEntry.go
@@ -5,8 +5,6 @@ import "github.com/BrandenWilliams/dubyah/libs/tasklists"
 type TasksEntry struct {
 	// EntryID for related tasklist
 	EntryID string `json:"entryID" form:"entryID"`
-	// Title of task
-	Title string `json:"title" form:"title"`
 	// The text of the task itself
 	TaskText string `json:"taskText" form:"taskText"`
 	// Task List position
@@ -16,8 +14,7 @@ type TasksEntry struct {
 }
 
 func (e TasksEntry) makeTasksEntry() (ae tasklists.Tasks) {
-	ae.Title = e.Title
-	ae.TaskText = e.Title
+	ae.TaskText = e.TaskText
 	ae.IsCompleted = e.IsCompleted
 	ae.TaskPosition = e.TaskPosition
 
