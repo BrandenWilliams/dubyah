3953b151aa8a1ec69c075c29a1b2b570cfad5725
BrandenWilliams <BrandenWilliams07@gmail.com>
2022-12-06 15:38:54 -0800

    remove console logs

--- public_html/js/httpRequest.js
+++ public_html/js/httpRequest.js
@@ -14,38 +14,28 @@ function getFetchOptions(method, data) {
 
 async function httpRequest(method, url, data) {
     const opts = getFetchOptions(method, data);
-    console.log("before fetch")
     const resp = await fetch(url, opts);
-    await resp.status
-    console.log("after fetch")
 
-    console.log("resp: ", resp)
     switch (resp.status) {
         case 200:
-            console.log("hi 200")
             return handleResp(resp);
         case 204:
-            console.log("hi 204")
             return null;
         case 400:
-            console.log("hi 400")
             return handleResp(resp);
         default:
             throw (`error encountered, status code of ${resp.status}`)
     }
 }
 
-
 const handleResp = async resp => {
     const parsed = await resp.json();
-    console.log("responce parsed")
     if (!!parsed.errors && parsed.errors.length > 0) {
         parsed.errors.forEach(notifyError);
         // TODO: Consider throwing here
         return null;
     }
 
-    console.log("data after parse: ", parsed.data)
     return parsed.data;
 };
 
