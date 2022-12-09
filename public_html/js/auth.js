227368f31a8b84a35ba8436d1273caa091f950d8
BrandenWilliams <BrandenWilliams07@gmail.com>
2022-12-08 16:38:08 -0800

    fix print

--- public_html/js/auth.js
+++ public_html/js/auth.js
@@ -15,7 +15,7 @@ function loginSubmit(evt) {
         }),
     })
         .then(response => response.json())
-        .then(json => console.log("response: " + json));
+        .then(json => json);
 }
 
 /*
